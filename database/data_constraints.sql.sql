f1faa695bb9f1c82e6c61015a453f92b3856ad48
1000Dat <166731709+1000Dat@users.noreply.github.com>
2026-03-02 15:17:30 +0700

    data contraints

---
+++ database/data_constraints.sql.sql
@@ -0,0 +1,18 @@
+﻿USE IndustrialSalesDB;
+GO
+
+-- Price phải > 0
+ALTER TABLE products
+ADD CONSTRAINT chk_price CHECK (price > 0);
+
+-- Cost >= 0
+ALTER TABLE products
+ADD CONSTRAINT chk_cost CHECK (cost >= 0);
+
+-- Quantity > 0
+ALTER TABLE order_details
+ADD CONSTRAINT chk_quantity CHECK (quantity > 0);
+
+-- Order date không được > ngày hiện tại
+ALTER TABLE orders
+ADD CONSTRAINT chk_order_date CHECK (order_date <= GETDATE());
